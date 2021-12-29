835c9c4d3124948091f6d604e438416ad1494352
lekavelucci <doremi2706@mail.ru>
2021-12-29 12:46:29 +0300

    Second commit

---
+++ sql/data.sql
@@ -0,0 +1,15 @@
+insert into usr (first_name, middle_name, last_name, email) values
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com'),
+('Анастасия', 'Эдуардовна', 'Кравчук', 'kravchuk@mail.ru'),
+('Артем', 'Дмитриевич', 'Мандрик','mandrik@mail.ru'),
+('Сергей', 'Сергеевич', 'Шульгин','shulgin@mail.ru '),
+('Максим', 'Валериевич','Баркин','barkin@mail.ru'),
+('Анастасия', 'Викторовна', 'Мухортова', 'muhortova@mail.ru'),
+('Алена', 'Андреевна', 'Бакшеева', 'baksheeva@mail.ru'),
+('Никита', 'Дмитриевич', 'Власов', 'vlasov@mail.ru'),
+('Наталья', 'Николаевна', 'Абрамова', 'abramova@mail.ru'),
+('Мария', 'Алексанровна', 'Минарди', 'meinaride@mail.ru'),
+('Илья', 'Владимирович', 'Раев', 'raev@mail.ru');
+SELECT * FROM usr;
