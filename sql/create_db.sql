c2a0a51f9635d799cf2bd79a1755031d1a9b3024
lekavelucci <doremi2706@mail.ru>
2021-12-29 11:32:19 +0300

    First commit

---
+++ sql/create_db.sql
@@ -0,0 +1,6 @@
+CREATE USER ice_chocolate WITH PASSWORD 'ice_nuts';
+CREATE DATABASE ice_db
+    WITH 
+    OWNER = ice_chocolate
+    ENCODING = 'UTF8'
+GRANT ALL PRIVILEGES ON DATABASE ice_db TO ice_chocolate;
