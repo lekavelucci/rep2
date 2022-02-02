b9ae538d609c34e73a9831688d79600c38da6daa
lekavelucci <doremi2706@mail.ru>
2022-02-02 14:44:15 +0300

    update files lab 13-14

--- sql/select_data.sql
+++ sql/select_data.sql
@@ -4,3 +4,13 @@ select *, LENGTH(first_name) as length from usr where length(first_name)>7;
 select id, birth_date from usr where birth_date > '2005-01-01';
 select id, first_name, last_name, birth_date from usr order by birth_date desc;
 select id, first_name, last_name, birth_date from usr where birth_date > '2003-01-01' and birth_date < '2008-01-01';
+
+
+select m.first_name, m.middle_name, m.last_name, m.e_mail, p.name from university_member um join usr m on um.user_id=m.id join position p on um.position_id=p.id;
+
+select m.first_name, m.middle_name, m.last_name, m.e_mail, g.name from group_member gm join university_member um on gm.university_member_id=um.user_id join group g on gm.group_id=g.id join usr m on um.user_id=m.id;
+
+
+
+select m.first_name, m.middle_name, m.last_name, m.e_mail, d.name 
+from university_member um join usr m on um.user_id=m.id join department d on um.departament_id=d. id;
